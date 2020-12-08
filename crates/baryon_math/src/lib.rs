39ede9be63c7e5275d182e31689f7b462a1f2642
Léo Masson <lmasson@edhebi.info>
2020-12-08 19:05:19 +0100

    add the baryon_math crate

---
+++ crates/baryon_math/src/lib.rs
@@ -0,0 +1,11 @@
+// SPDX-FileCopyrightText: 2020 Léo Masson <lmasson@edhebi.info>
+//
+// SPDX-License-Identifier: Zlib
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
